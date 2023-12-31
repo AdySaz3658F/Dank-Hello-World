7a63560bd0185d92f01df15f64b99a9c965cdb6c
AdySaz3658F <adyrocks31@gmail.com>
2023-12-31 22:53:16 +0530

    HelloWorld.cpp asks user about their favourite color and displays it, just testing emergency fix branch method

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main()
 {
+    char colors[50];
+
     cout << "Hello World, me learning Git";
+    cout << "Enter your favorite color: ";
+    cin >> colors;
+    cout << "your favourite color is: " << colors;
 }
